050dd40735b81812bfdb6d8099a1179892cf4867
sameencai <54757010+sameencai@users.noreply.github.com>
2019-10-31 21:05:01 +0800

    update 10312104
    update assign7

--- assignment7/script.js
+++ assignment7/script.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
 
     $("button#hide_h2").click(function() {
@@ -15,6 +16,7 @@ $(document).ready(function(){
         $x.empty();
     });
 
+//chart
     $("button#get_data").click(function() {
         var items = [];
         var i = 0;
@@ -83,7 +85,7 @@ $(document).ready(function(){
                data: dataSet,
                retrieve: true,
                columns: [
-                   { title: "Name",
+                   { title: "Industry",
                      defaultContent:""},
                    { title: "Averagesalary",
                        defaultContent:"" },
@@ -109,3 +111,41 @@ $(document).ready(function(){
    }); // end button
 
 }); // document ready
+
+
+//map
+var mapboxTiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: 'Map data &copy; <a href="https://openstreetmap.org">OpenStreetMap</a>', maxZoom: 18,});
+
+var map = L.map('map')
+      .addLayer(mapboxTiles)
+      .setView([22.615000, 114.159700], 10);
+
+var items = [];
+var airtable_read_endpoint = "https://api.airtable.com/v0/apptYpOzvQlg0TiA6/1?api_key=keyAWB8coCDESNvtz";
+
+var data = [];
+$.getJSON(airtable_read_endpoint, function(result) {
+       $.each(result.records, function(key,value) {
+           items = {};
+               items["name"] = value.fields.Name;
+               items["url"] = value.fields.url;
+               items["image_url"] = value.fields.img_url;
+               items["latitud"] = value.fields.Lat;
+               items["longitud"] = value.fields.Lng;
+               data.push(items);
+               console.log(items);
+        }); // end .each
+}); // end getJSON
+
+function show_districts(){
+
+  for (var i in data) {
+      var latlng = L.latLng({ lat: data[i].latitud, lng: data[i].longitud });
+      L.marker( latlng )
+          .bindPopup( '<a href="' + data[i].url + '" target="_blank">' + '<img src="' + data[i].image_url+'" width = "80px"><br>'+data[i].name + '</a>' )
+          .addTo(map);
+  }
+
+}
+
+
