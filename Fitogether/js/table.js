a3c58b21028ea0295cc21a23e773616f4ac3c485
sameencai <54757010+sameencai@users.noreply.github.com>
2019-12-02 21:02:57 +0800

    back

---
+++ Fitogether/js/table.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+
+    $("button#hide_h2").click(function() {
+        $("h2").hide(500);
+    });
+
+    $("button#show_h2").click(function() {
+        $("h2").show(300);
+        $("h2").css("color","blue");
+        $("h2").html("You clicked me hard.");
+    });
+
+    $("button#clear_screen").click(function() {
+        var $x = $("container");
+        $x.empty();
+    });
+
+    $("button#get_data").click(function() {
+        var items = [];
+        var i = 0;
+        var airtable_read_endpoint = "https://api.airtable.com/v0/appXSoGTDQ6IdE0Iu/CompanyInformation?api_key=keyiUoXoMYkTNkr4G";
+       var dataSet = [];
+        $.getJSON(airtable_read_endpoint, function(result) {
+               $.each(result.records, function(key,value) {
+                   items = [];
+                       items.push(value.fields.Name);
+                       items.push(value.fields.rating);
+                       items.push(value.fields.fullname);
+                       items.push(value.fields.industry);
+                       items.push(value.fields.industryType);
+                       items.push(value.fields.scale);
+                       items.push(value.fields.base);
+                       items.push(value.fields.interviewdifficulty);
+                       items.push(value.fields.averagesalary);
+                       dataSet.push(items);
+                       console.log(items);
+                }); // end .each
+                console.log(dataSet);
+
+             $('#table1').DataTable( {
+                 data: dataSet,
+                 retrieve: true,
+                 columns: [
+                     { title: "Name",
+                       defaultContent:""},
+                     { title: "Rating",
+                         defaultContent:"" },
+                     { title: "Fullname",
+                       defaultContent:"" },
+                     { title: "Industry",
+                       defaultContent:""},
+                     { title: "Industry Type",
+                         defaultContent:""},
+                     { title: "Scale",
+                       defaultContent:""},
+                     { title: "Base",
+                       defaultContent:""},
+                     { title: "Interview Difficulty",
+                       defaultContent:""},
+                     { title: "Average Salary",
+                       defaultContent:""},
+                 ]
+             } );
+        }); // end .getJSON
+     }); // end button
+
+}); // document ready
